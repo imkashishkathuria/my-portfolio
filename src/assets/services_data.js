01e489a53a7cc32d921dd2a0a42e6428f1ada53f
Kashish <kathuriak805@gmail.com>
2025-01-24 21:38:21 +0530

    my services section

--- src/assets/services_data.js
+++ src/assets/services_data.js
@@ -1,33 +1,33 @@
 const Services_Data = [
     {
         s_no:"01",
-        s_name:"Web design",
-        s_desc:"Web development is the process of building, programming..."
+        s_name:"Responsive Web Development",
+        s_desc:"I specialize in creating responsive websites that provide a seamless experience across all devices. Using modern frameworks like React.js and tools like Tailwind CSS, I ensure every design looks great and works perfectly on screens of all sizes."
     },
     {
         s_no:"02",
-        s_name:"Graphics design",
-        s_desc:"Web development is the process of building, programming..."
+        s_name:"Dynamic User Interfaces",
+        s_desc:"I build interactive and engaging user interfaces using React.js. With a focus on usability and performance, I aim to deliver smooth, intuitive, and enjoyable experiences for end-users."
     },
     {
         s_no:"03",
-        s_name:"Social media",
-        s_desc:"Web development is the process of building, programming..."
+        s_name:"Single-Page Applications (SPAs)",
+        s_desc:"I develop efficient and fast single-page applications using React.js, ensuring seamless navigation and a dynamic user experience without unnecessary page reloads."
     },
     {
         s_no:"04",
-        s_name:"App design",
-        s_desc:"Web development is the process of building, programming..."
+        s_name:"Custom Component Development",
+        s_desc:"I create reusable, modular, and scalable React components tailored to fit your project’s unique needs. This approach ensures maintainable code and consistency across your application."
     },
     {
         s_no:"05",
-        s_name:"Digital marketing",
-        s_desc:"Web development is the process of building, programming..."
+        s_name:"API Integration",
+        s_desc:"I integrate third-party APIs into web applications to fetch and display dynamic data. Whether it's fetching data from REST APIs or handling asynchronous tasks, I ensure smooth and efficient communication between the client and the server"
     },
     {
         s_no:"06",
-        s_name:"Content writing",
-        s_desc:"Web development is the process of building, programming..."
+        s_name:"Debugging and Optimization",
+        s_desc:"I focus on writing clean, optimized code to ensure websites and applications run efficiently. I identify and resolve performance issues to improve the overall user experience."
     },
 ]
  

60101c813f9f14d938dd25c1fac63907c3230bc9
Kashish <kathuriak805@gmail.com>
2025-01-25 12:52:48 +0530

    my work section

---
+++ src/Components/MyWork/MyWork.jsx
@@ -0,0 +1,33 @@
+import { Link } from 'react-router-dom';
+import mywork_data from '../../assets/mywork_data';
+import theme_pattern from '../../assets/theme_pattern.svg'
+import './MyWork.css';
+import arrow_icon from '../../assets/arrow_icon.svg'
+
+
+const MyWork = () => {
+    
+  return (
+    <div className='mywork'>
+        <div className="mywork-title">
+            <h1>My latest work</h1>
+            <img src={theme_pattern} alt='' />
+
+        </div>
+        <div className="mywork-container">
+            {
+                mywork_data.map((work, index)=>{
+                    return <a href={work.w_link} target='_blank' rel='noopener noreferrer'><img key={index} src={work.w_img} alt='' width="350px" height="250px"/></a>
+                })
+            }
+        </div>
+        <div className="mywork-showmore">
+            <p>Show More</p>
+            <img src={arrow_icon} alt='' />
+        </div>
+      
+    </div>
+  )
+}
+
+export default MyWork
--- src/assets/mywork_data.js
+++ src/assets/mywork_data.js
@@ -4,37 +4,48 @@ import project3_img from '../assets/project_3.svg'
 import project4_img from '../assets/project_4.svg'
 import project5_img from '../assets/project_5.svg'
 import project6_img from '../assets/project_6.svg'
+import picture1 from '../assets/picture1.PNG'
+import picture2 from '../assets/picture2.PNG'
+import picture3 from '../assets/picture3.PNG'
+import picture4 from '../assets/picture4.PNG'
+import picture5 from '../assets/picture5.PNG'
+import picture6 from '../assets/picture6.PNG'
 
 const mywork_data = [
     {
         w_no:1,
-        w_name:"Web design",
-        w_img:project1_img
+        w_name:"Spotify Clone",
+        w_img:picture1,
+        w_link:"https://spotify-clone-seven-gilt.vercel.app/"
     },
     {
         w_no:2,
-        w_name:"Web design",
-        w_img:project2_img
+        w_name:"Shopping Cart",
+        w_img:picture2,
+        w_link:"https://shopping-cart-six-snowy-68.vercel.app/"
     },
     {
         w_no:3,
-        w_name:"Web design",
-        w_img:project3_img
+        w_name:"Google Gemini Clone",
+        w_img:picture3,
+        w_link:"https://gemini-clone-mu-gray.vercel.app/"
     },
     {
         w_no:4,
-        w_name:"Web design",
-        w_img:project4_img
+        w_name:"Wei Flix",
+        w_img:picture4,
+        w_link:"https://wei-flix.vercel.app/"
     },
     {
         w_no:5,
-        w_name:"Web design",
-        w_img:project5_img
+        w_name:"Weather Forecast App",
+        w_img:picture5,
+        w_link:"https://weather-forecast-eosin-nu.vercel.app/"
     },
     {
         w_no:6,
-        w_name:"Web design",
-        w_img:project6_img
+        w_name:"Netflix Clone",
+        w_img:picture6
     },
 ]
  

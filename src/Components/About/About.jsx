9852244653f30ba1b892ef0ca658454a37607c56
Kashish Kathuria <112476908+imkashishkathuria@users.noreply.github.com>
2025-07-16 12:20:49 +0530

    Update About.jsx

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -15,7 +15,7 @@ const About = () => {
             </div>
             <div className="about-right">
                 <div className="about-para">
-                    <p>I’m an aspiring frontend developer with a strong passion for building engaging, user-friendly web interfaces. Skilled in React.js, I love turning creative ideas into responsive, dynamic websites that provide seamless user experiences. Although I’m just starting my professional journey, I’ve spent significant time honing my skills through personal projects, online courses, and hands-on coding challenges.</p>
+                    <p>I’m an aspiring software developer with a strong passion for building engaging, user-friendly web interfaces. Skilled in React.js and NextJS, I love turning creative ideas into responsive, dynamic websites that provide seamless user experiences. Although I’m just starting my professional journey, I’ve spent significant time honing my skills through personal projects, online courses, and hands-on coding challenges.</p>
                     <p>I’m eager to collaborate with experienced developers, learn from industry experts, and grow in a team environment where I can refine my skills and bring value to innovative projects. </p>
                 </div>
                 <div className="about-skills">
@@ -28,7 +28,7 @@ const About = () => {
                         <hr style={{width: "75%"}} />
                     </div>
                     <div className="about-skill">
-                        <p>JavaScript</p>
+                        <p>NextJS</p>
                         <hr style={{width: "50%"}} />
                     </div>
                     <div className="about-skill">
@@ -42,12 +42,12 @@ const About = () => {
         </div>
         <div className="about-achievements">
                 <div className="about-achievement">
-                    <h1>3 months</h1>
+                    <h1>4 months</h1>
                     <p>INTERNSHIP EXPERIENCE</p>
                 </div>
                 <hr />
                 <div className="about-achievement">
-                    <h1>10+</h1>
+                    <h1>12+</h1>
                     <p>PROJECTS COMPLETED</p>
                 </div>
                 <hr />
